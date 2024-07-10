a14f5a464e32d50eb36811ca50d07360008c68b2
OmG2807 <139834596+OmG2807@users.noreply.github.com>
2024-07-10 18:26:37 +0530

    Update BackPropogation.py

--- BackPropogation.py
+++ BackPropogation.py
@@ -1,62 +1,19 @@
 import numpy as np
-x=np.array(([2,9],[1,5],[3,6]),dtype=float)
-y=np.array(([92],[86],[89]),dtype=float)
-x=x/np.amax(x,axis=0)
-y=y/100
-
-def sigmoid(x):
-    return 1/(1+np.exp(-x))
-
-def derivaties_sigmoid(x):
-    return x*(1-x)
-epoch=5000
-lr=0.1
-inputlayer_neurons=2
-hiddenlayer_neurons=3
-output_neurons=1
-wh=np.random.uniform(size=(inputlayer_neurons,hiddenlayer_neurons))
-bh=np.random.uniform(size=(1,hiddenlayer_neurons))
-
-wout=np.random.uniform(size=(hiddenlayer_neurons,output_neurons))
-
-bout=np.random.uniform(size=(1,output_neurons))
-
-for i in range(epoch):
-    hinp1=np.dot(x,wh)
-    hinp=hinp1+bh
-    hlayer_act=sigmoid(hinp)
-    outinp1=np.dot(hlayer_act,wout)
-    outinp=outinp1+bout
-    output=sigmoid(outinp)
-    
-
-    eo=y-output
-    outgrad=derivaties_sigmoid(output)
-    d_output=eo*outgrad
-    eh=d_output.dot(wout.T)
-    
-    
-    hiddengrad=derivaties_sigmoid(hlayer_act)
-    d_hiddenlayer=eh*hiddengrad
-    
-    wout+=hlayer_act.T.dot(d_output)*lr
-    wh+=x.T.dot(d_hiddenlayer)*lr
-    
-print("Input:\n"+str(x))
-print("Actual Output :\n"+str(y))
-print("Predicted Output :\n",output)
-    
-
-# OUTPUT:
-# Inpput:
-# [[0.66666667 1.        ]
-#  [0.33333333 1.        ]
-#  [1.         0.66666667]]
-# Actual:
-# [[0.92]
-#  [0.86]
-#  [0.89]]
-# Predicted:
-#  [[0.89155219]
-#  [0.88386888]
-#  [0.89127146]]                               
+x = np.array([[2, 9], [1, 5], [3, 6]], dtype=float) / np.amax(np.array([[2, 9], [1, 5], [3, 6]], dtype=float), axis=0)
+y = np.array([[92], [86], [89]], dtype=float) / 100
+sigmoid = lambda x: 1 / (1 + np.exp(-x))
+derivatives_sigmoid = lambda x: x * (1 - x)
+epoch, lr = 5000, 0.1
+inputlayer_neurons, hiddenlayer_neurons, output_neurons = 2, 3, 1
+wh, bh = np.random.uniform(size=(inputlayer_neurons, hiddenlayer_neurons)), np.random.uniform(size=(1, hiddenlayer_neurons))
+wout, bout = np.random.uniform(size=(hiddenlayer_neurons, output_neurons)), np.random.uniform(size=(1, output_neurons))
+for _ in range(epoch):
+    hlayer_act = sigmoid(np.dot(x, wh) + bh)
+    output = sigmoid(np.dot(hlayer_act, wout) + bout)  
+    d_output = (y - output) * derivatives_sigmoid(output)
+    d_hiddenlayer = d_output.dot(wout.T) * derivatives_sigmoid(hlayer_act)
+    wout += hlayer_act.T.dot(d_output) * lr
+    wh += x.T.dot(d_hiddenlayer) * lr
+print(f"Input:\n{x}")
+print(f"Actual Output:\n{y}")
+print(f"Predicted Output:\n{output}")
